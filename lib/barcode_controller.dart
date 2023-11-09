d0bf04f922890cd9d6267ddc2b9794fa2ae7a29f
Teerasej <teerasej@gmaill.com>
2023-11-09 23:16:52 +0700

    finished

--- lib/barcode_controller.dart
+++ lib/barcode_controller.dart
@@ -2,7 +2,22 @@ import 'package:get/get.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class BarcodeController extends GetxController {
-  var _barcodeValue = "".obs;
+  var barcodeValue = "".obs;
 
-  startScan() async {}
+  startScan() async {
+    // เรียกใช้ตัวแสกน
+    String barcodeScanResult = await FlutterBarcodeScanner.scanBarcode(
+      // กำหนดสีเส้นแสกน
+      '#FF0000',
+
+      // ข้อความบนปุ่มยกเลิก
+      'Cancel',
+
+      // เปิด/ปิดใช้งานปุ่มไฟฉาย
+      false,
+      ScanMode.DEFAULT,
+    );
+
+    barcodeValue.value = barcodeScanResult;
+  }
 }
